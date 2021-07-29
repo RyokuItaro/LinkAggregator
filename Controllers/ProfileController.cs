845469c187272de0acead7e8afbd3aab8015600d
KarolK <kkacprzak2115@gmail.com>
2021-07-29 14:50:17 +0200

    Validation added, Profile site added, Red button of desctruction removed (?what?)

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,30 @@
+﻿using LinkAggregator.Models;
+using LinkAggregator.Models.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkAggregator.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ILinkRepository _linkRepository;
+        private readonly UserManager<UserEntity> _userManager;
+
+        public ProfileController(ILinkRepository linkRepository, UserManager<UserEntity> userManager)
+        {
+            _linkRepository = linkRepository;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index(int pageNumber = 1)
+        {
+            var links = _linkRepository.AllLinksQueryable.OrderByDescending(l => l.Points);
+            return View(await PaginatedLinkList<LinkEntity>.CreateAsync(links, pageNumber, 10));
+        }
+    }
+}
--- Models/LinkInputModel.cs
+++ Models/LinkInputModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,12 @@ namespace LinkAggregator.Models
 {
     public class LinkInputModel
     {
+        [Required]
+        [RegularExpression(@"(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))",
+            ErrorMessage = "That link is not valid! Make sure that it starts with: \"https://www.\" or \"http://www.\"")]
         public string Url { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Title { get; set; }
 
     }
